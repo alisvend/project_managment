e3c6c67093971b0fec4f3fca9573df32a440e73e
alisvend <ali.j.alhajj@gmail.com>
2020-11-26 11:00:18 +0200

    Bootstrap commit

--- Frontend/src/components/Projects.js
+++ Frontend/src/components/Projects.js
@@ -1,39 +1,33 @@
 import React from 'react';
 import apiClient from '../services/api';
 
-export default class Projects extends React.Component {
-        constructor(props) {
-            super(props);
-            this.state = {
-                projects: [],
-                
-    
-            };
+const Projects = (props) => {
+    const [projects, setProjects] = React.useState([]);
+    React.useEffect(() => {
+        if (props.loggedIn) {
+            apiClient.get('sanctum/csrf-cookie').then(() => apiClient.get('/api/projects')
+                .then(response => {
+                    setProjects(response.data.data)
+                })
+                .catch(error => console.error(error)))
 
         }
+    }, []);
+    const projectList = projects.map((project) =>
+        <option value={project.id} key={project.id} >{project.projectName}</option>
 
+    );
+    if (props.loggedIn) {
+        return (
 
-        fetchProjects() {
-                    if (this.props.loggedIn) {
-                        apiClient.get('sanctum/csrf-cookie').then(() => apiClient.get('/api/projects')
-                            .then(response => {
-                                const projects = response.data.data;
-                                this.setState({ projects: projects });
-                            })
-                            .catch(error => console.error(error)
-                            ))
-            
-                    }
-                }
-                componentDidMount = (props) => {
-                    this.fetchProjects();
-                }
-
-
-       
-    render(){
-
-
-        return( <div className="alert alert-warning">You are logged in.</div>);
+            <select className="custom-select" onChange={(e) => { props.onChangeProjId(e.target.value) }} >
+                <option value={0}>Show Projects</option>{projectList}
+            </select>
+        );
     }
-    }
+    return (
+        <div className="alert alert-warning">You are not logged in.</div>
+    );
+};
+
+export default Projects;
